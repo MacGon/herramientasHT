ce86317fe7a1e9cf5c062308e9d00df3ce2effae
Manuel <manuel.cardenas@elektra.com.mx>
2019-08-01 16:42:08 -0500

    Se agrega barra de navegación nativa

--- app/src/main/java/com/simaht/modules/dashboard_mh/MainActivity.kt
+++ app/src/main/java/com/simaht/modules/dashboard_mh/MainActivity.kt
@@ -105,9 +105,16 @@ class MainActivity: AppCompatActivity(), NavigationView.OnNavigationItemSelected
         toggle.syncState()
         navViewTwo.setNavigationItemSelectedListener(this)
 //////////////Revisar BaseActivity
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-        val decorView = window.decorView
-        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+         //window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
+        window.decorView.apply {
+            // Hide both the navigation bar and the status bar.
+            // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+            // a general rule, you should design your app to hide the status bar whenever you
+            // hide the navigation bar.
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        }
+
 ///////////
     }
 
